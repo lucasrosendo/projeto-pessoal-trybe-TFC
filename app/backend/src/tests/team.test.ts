80ae2fc2e04639500615f8895490092b2caab111
lucasrosendo <lucasrosendo91@gmail.com>
2022-07-20 23:02:23 -0300

    tests teams

---
+++ app/backend/src/tests/team.test.ts
@@ -0,0 +1,23 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('All Teams', () => {
+  it('Verifica se o end point retorna status 200', async () => { 
+    const response = await chai.request(app).get('/teams');
+
+    expect(response).to.status(200);
+  });
+  it('Verifica o corpa da resposta com os teams', async () => {
+    const response = await chai.request(app).get('/teams');
+    expect(response.body).to.have.length(16);
+  });
+});
+
